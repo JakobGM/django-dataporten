7561573e8d6a955f702e9602a91b07d95c65a6d8
Jakob Gerhard Martinussen <jakobgm@gmail.com>
2019-01-06 16:36:24 +0100

    Support django 2.0 middleware API

--- dataporten/tests/test_middleware.py
+++ dataporten/tests/test_middleware.py
@@ -1,3 +1,5 @@
+from unittest.mock import Mock
+
 from django.contrib.auth.models import AnonymousUser, User
 
 import pytest
@@ -6,30 +8,38 @@ from ..middleware import DataportenGroupsMiddleware
 from ..models import DataportenUser
 
 
-def test_dataporten_middleware_with_anonymous_user(rf):
+def test_dataporten_middleware_with_anonymous_user(rf, monkeypatch):
     request = rf.get('')
-    dpm = DataportenGroupsMiddleware(None)
+    dpm = DataportenGroupsMiddleware(Mock())
 
     request.user = AnonymousUser()
-    dpm.process_view(request)
+    dpm(request)
     assert type(request.user) is AnonymousUser
 
 
 @pytest.mark.django_db
-def test_dataporten_middleware_with_plain_django_user(rf, django_user_model):
+def test_dataporten_middleware_with_plain_django_user(
+    rf,
+    django_user_model,
+    monkeypatch,
+):
     request = rf.get('')
-    dpm = DataportenGroupsMiddleware(None)
+    dpm = DataportenGroupsMiddleware(Mock())
 
     request.user = django_user_model()
-    dpm.process_view(request)
+    dpm(request)
     assert type(request.user) is User
 
 
 @pytest.mark.django_db
-def test_dataporten_middleware_with_user_with_dataporten_credentials(rf, user_with_dataporten_token):
+def test_dataporten_middleware_with_user_with_dataporten_credentials(
+    rf,
+    user_with_dataporten_token,
+    monkeypatch,
+):
     request = rf.get('')
-    dpm = DataportenGroupsMiddleware(None)
+    dpm = DataportenGroupsMiddleware(Mock())
 
     request.user = user_with_dataporten_token
-    dpm.process_view(request)
+    dpm(request)
     assert type(request.user) is DataportenUser
